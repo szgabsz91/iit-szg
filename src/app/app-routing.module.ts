b98817c2d40208753b3b5da4aa6f50bfc633266b
Gabor Szabo <szgabsz91@gmail.com>
2020-03-26 20:52:19 +0100

    feat: #25 delete the unnecessary CourseParentComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CourseParentComponent } from './course-parent/course-parent.component';
 
 const routes: Routes = [{
   path: '',
@@ -9,7 +8,6 @@ const routes: Routes = [{
   component: HomeComponent
 }, {
   path: 'courses/:courseId',
-  component: CourseParentComponent,
   children: [{
     path: '',
     loadChildren: () => import('./course/course.module').then(m => m.CourseModule)
