0475fcc97f27d14c3fd6d7df8d5c7f67dc1d5e57
Gabor Szabo <szgabsz91@gmail.com>
2020-04-18 21:14:29 +0200

    feat: #96 redirect the browser on Github Pages if a subpage is open, and support hard refresh

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Github Pages hard refresh fix
+let { redirect } = sessionStorage;
+delete sessionStorage.redirect;
+if (redirect && redirect !== location.href) {
+  const localeId = location.pathname.indexOf('/hu/') > -1 ? 'hu' : 'en';
+  const otherLocaleId = localeId === 'en' ? 'hu' : 'en';
+  redirect = redirect.replace('/' + otherLocaleId + '/', '/' + localeId + '/');
+  history.replaceState(null, null, redirect);
+}
+
 if (environment.production) {
   enableProdMode();
 }
