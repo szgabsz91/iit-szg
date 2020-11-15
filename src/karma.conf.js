b81ae2516b218cf6f4e5357d623d38825a53c5d8
Gabor Szabo <szgabsz91@gmail.com>
2020-11-15 16:06:54 +0100

    feat: #123 replace karma-coverage-istanbul-reporter with karma-coverage

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -9,24 +9,28 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
+      type: 'html',
       dir: require('path').join(__dirname, '../coverage/iit-www'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true,
-      thresholds: {
-        statements: 100,
-        lines: 100,
-        branches: 100,
-        functions: 100
+      check: {
+        each: {
+          statements: 100,
+          lines: 100,
+          branches: 100,
+          functions: 100
+        }
       }
     },
     reporters: ['progress', 'kjhtml'],
+    preprocessors: {
+      '**/*.js': ['coverage']
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
