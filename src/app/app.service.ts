a0c06203536b00c063d681c8cca4af3bd61d5d7d
Gabor Szabo <szgabsz91@gmail.com>
2021-06-14 22:17:40 +0200

    fix: #174 implement cache busting for metadata.json in prod mode

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { METADATA_FILENAME } from './constants/app.constants';
 import { Metadata } from './model/metadata';
 import { Course } from './model/course';
 
@@ -10,7 +11,7 @@ import { Course } from './model/course';
 })
 export class AppService {
 
-  private readonly courseCache$: Observable<Course[]> = this.httpClient.get<Metadata>('./assets/metadata.json').pipe(
+  private readonly courseCache$: Observable<Course[]> = this.httpClient.get<Metadata>(`./assets/${METADATA_FILENAME}`).pipe(
     map(metadata => metadata.courses),
     shareReplay(1)
   );
