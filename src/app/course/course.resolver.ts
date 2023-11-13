f4c06c64113b58feadc0d3c8535c9b34ed4946da
Gabor Szabo <szgabsz91@gmail.com>
2023-11-13 22:07:40 +0100

    feat: #300 upgrade Angular core and CLI to version 16

--- src/app/course/course.resolver.ts
+++ src/app/course/course.resolver.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Course } from '../model/course';
 import { CourseService } from './course.service';
 
 @Injectable()
-export class CourseResolver implements Resolve<Course> {
+export class CourseResolver {
   constructor(private readonly courseService: CourseService) {}
 
   resolve(activatedRouteSnapshot: ActivatedRouteSnapshot): Observable<Course> {
--- src/app/lab/lab.resolver.ts
+++ src/app/lab/lab.resolver.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Lab } from '../model/lab';
 import { LabService } from './lab.service';
 
 @Injectable()
-export class LabResolver implements Resolve<Lab> {
+export class LabResolver {
   constructor(private readonly labService: LabService) {}
 
   resolve(activatedRouteSnapshot: ActivatedRouteSnapshot): Observable<Lab> {
